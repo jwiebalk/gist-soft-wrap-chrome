615be3890d183e7fc0675fcae3d4d247376cfa6a
John Wiebalk <jwiebalk@github.com>
2017-02-27 14:23:43 -0500

    maybe working?

--- gistSoftWrap.js
+++ gistSoftWrap.js
@@ -1,8 +1,12 @@
 function changeContent(){
     return function() {
 var element = document.getElementsByClassName('form-select select-sm js-code-wrap-mode');
+
 element[0].options.selectedIndex = 1;
+
+element[0].form[11].nextElementSibling.className += ' CodeMirror-wrap';
 }
 }
 
-setTimeout(changeContent(), 100)
+
+setTimeout(changeContent(), 1000)
